b7f4cbf698fd3f7b2f94c97fa9dec68c3117550c
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2024-11-05 12:25:16 +0600

    общая структура

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+// eslint.config.js
+module.exports = [
+  {
+    files: ['**/*.js'],
+    rules: {
+      'no-console': 'warn',
+    },
+  },
+  // Другие правила конфигурации могут быть добавлены здесь
+];
